6c04be1ca4fc85dede309471fed5ecbb39101bf4
Katulos <katulos@protonmail.com>
2025-02-13 15:54:58 +0300

    refactor: Refactoring example

--- examples/example.py
+++ examples/example.py
@@ -1,15 +1,23 @@
-from __future__ import annotations
+import asyncio
+import logging
+import sys
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher.storage import FSMContext
-from config import API_TOKEN
+from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardRemove,
+)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from sqlitestorage.storage import SQLiteStorage
 
-bot = Bot(token=API_TOKEN)
-storage = SQLiteStorage()
-dp = Dispatcher(bot, storage=storage)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+API_TOKEN = "YOUR_TOKEN_HERE"
+
+router = Router()
 
 
 class Phase(StatesGroup):
@@ -18,29 +26,32 @@ class Phase(StatesGroup):
     FIN = State()
 
 
-@dp.message_handler(text_startswith="Get values", state=Phase.SECOND)
-async def get_value(message: types.Message, state: FSMContext):
-    await message.answer(f"State is {await state.get_state()}")
+@router.message(F.text.startswith("Get values"), Phase.SECOND)
+async def get_value_phase_second(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    await message.answer(f"State is {current_state}")
+
     data = await state.get_data()
     text = "\n".join(f"{key}: {value}" for key, value in data.items())
     await message.answer(
-        "Values of data is:\n" + text,
-        reply_markup=types.ReplyKeyboardRemove(),
+        f"Values of data is:\n{text}",
+        reply_markup=ReplyKeyboardRemove(),
     )
-    await Phase.FIN.set()
+
+    await state.set_state(Phase.FIN)
 
 
-@dp.message_handler(state=Phase.FIN)
-async def get_value(message: types.Message, state: FSMContext):
+@router.message(Phase.FIN)
+async def get_value_phase_fin(message: types.Message, state: FSMContext):
     await message.answer(f"State is {await state.get_state()}")
     if message.text == "/start":
-        await Phase.START.set()
+        await state.set_state(Phase.START)
     else:
         await message.answer("Good bye!")
 
 
-@dp.message_handler(state=Phase.SECOND)
-async def save_value(message: types.Message, state: FSMContext):
+@router.message(Phase.SECOND)
+async def save_value_phase_second(message: types.Message, state: FSMContext):
     await message.answer(f"State is {await state.get_state()}")
     if "=" not in message.text:
         await message.answer("Enter pair of args in format: <key>=<values>")
@@ -50,20 +61,22 @@ async def save_value(message: types.Message, state: FSMContext):
     data = {key: value}
     await state.update_data(**data)
 
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=False)
-    await message.answer(
-        f"Saved! {key}: {value}.",
-        reply_markup=markup.add(types.KeyboardButton("Get values")),
-    )
-    await Phase.SECOND.set()
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text="Get values"))
+    builder.adjust(1)
+
+    markup = builder.as_markup(one_time_keyboard=False, resize_keyboard=True)
+
+    await message.answer(f"Saved! {key}: {value}.", reply_markup=markup)
+    await state.set_state(Phase.SECOND)
 
 
-@dp.message_handler(content_types=types.ContentTypes.TEXT, state="*")
-async def save_value(message: types.Message, state: FSMContext):
+@router.message()
+async def save_value_phase_all(message: types.Message, state: FSMContext):
     await message.answer(f"State is {await state.get_state()}")
     if "=" not in message.text:
         await message.answer("Enter pair of args in format: <key>=<values>")
-        await Phase.START.set()
+        await state.set_state(Phase.START)
         return
 
     key, value = message.text.split("=")
@@ -74,14 +87,33 @@ async def save_value(message: types.Message, state: FSMContext):
         f"Saved! {key}: {value}.\nYou can add new pair of key-value to data"
     )
     await message.answer(text)
-    await Phase.SECOND.set()
+    await state.set_state(Phase.SECOND)
 
 
-async def shutdown(dp: Dispatcher):
+async def shutdown(
+    dp: Dispatcher,
+    bot: Bot,
+) -> None:
+    await bot.session.close()
     await dp.storage.close()
     await dp.storage.wait_closed()
-    print("Storage is closed!")
+    logging.info("Storage is closed!")
+
+
+def main():
+    dp = Dispatcher(storage=SQLiteStorage())
+    bot = Bot(
+        token=API_TOKEN,
+    )
+
+    dp.include_router(router)
+    dp.shutdown.register(shutdown)
+
+    asyncio.run(dp.start_polling(bot))
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_shutdown=shutdown)
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot stopped")
