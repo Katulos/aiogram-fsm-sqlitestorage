3c63fb92464b948d372eba062c385e5081668f51
Katulos <katulos@protonmail.com>
2025-02-13 13:52:44 +0300

    refactor: Refactoring tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
-from __future__ import annotations
-
 import os
 from typing import AsyncGenerator
 
 import bson
 import pytest
+from aiogram.fsm.storage.base import StorageKey
 
 from sqlitestorage.storage import SQLiteStorage
 
 
-@pytest.fixture()
+@pytest.fixture()  # type: ignore
 async def storage() -> AsyncGenerator[SQLiteStorage, None]:
     db_filename = str(bson.ObjectId()) + ".db"
     database = SQLiteStorage(db_path=db_filename)
@@ -17,5 +16,10 @@ async def storage() -> AsyncGenerator[SQLiteStorage, None]:
         yield database
     finally:
         await database.close()
-        await database.wait_closed()
+        # await database.wait_closed() #raise PermissionError
         os.remove(db_filename)
+
+
+@pytest.fixture()  # type: ignore
+async def storage_key() -> StorageKey:
+    return StorageKey(bot_id=123456789, chat_id=123, user_id=456)
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,32 +1,29 @@
-from __future__ import annotations
-
-from aiogram.fsm.storage.base import BaseStorage
+from aiogram.fsm.storage.base import BaseStorage, StorageKey
 
 
 async def test_set_get(
     storage: BaseStorage,
+    storage_key: StorageKey,
 ) -> None:
-    assert await storage.get_data() == {}
-    await storage.set_data(data={"foo": "bar"})
-    assert await storage.get_data() == {"foo": "bar"}
+    assert await storage.get_data(storage_key) == {}
+    await storage.set_data(storage_key, data={"foo": "bar"})
+    assert await storage.get_data(storage_key) == {"foo": "bar"}
 
 
 async def test_reset(
     storage: BaseStorage,
+    storage_key: StorageKey,
 ) -> None:
-    await storage.set_data(data={"foo": "bar"})
-    await storage.set_state(state="SECOND")
+    await storage.set_data(storage_key, data={"foo": "bar"})
+    await storage.set_state(storage_key, state="SECOND")
 
-    await storage.reset_data()
-    assert await storage.get_state() == "SECOND"
-    await storage.set_data(data={"foo": "bar"})
-
-    await storage.reset_state()
-    assert await storage.get_data() == {"foo": "bar"}
+    await storage.set_data(storage_key, data={})
+    assert await storage.get_state(storage_key) == "SECOND"
 
 
 async def test_reset_empty(
     storage: BaseStorage,
+    storage_key: StorageKey,
 ) -> None:
-    await storage.reset_data()
-    assert await storage.get_data() == {}
+    await storage.set_data(storage_key, data={})
+    assert await storage.get_data(storage_key) == {}
